37c219d4a5071de6424054c998d6b2d2d0801945
SH-Arrow <shurt91112@gmail.com>
2018-01-20 00:11:33 -0500

    SH: xcode project base

---
+++ Zippy Contact/Zippy Contact/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Zippy Contact
+//
+//  Created by Steven Hurtado on 1/19/18.
+//  Copyright © 2018 Steven Hurtado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
