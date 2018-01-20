4f17bd82751b8e018a13e676c431942e105563fc
SH-Arrow <shurt91112@gmail.com>
2018-01-20 15:33:08 -0500

    SH: heyo looks better-o

--- Zippy Contact/Zippy Contact/CanvasViewController.swift
+++ Zippy Contact/Zippy Contact/CanvasViewController.swift
@@ -15,7 +15,16 @@ class CanvasViewController: UIViewController {
     @IBOutlet weak var imgView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.setUpView()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func setUpView() {
+        self.imgView.layer.shadowColor = UIColor.black.cgColor
+        self.imgView.layer.shadowOpacity = 0.48
+        self.imgView.layer.shadowOffset = CGSize(width: 0.0, height: 10.0)
+        self.imgView.layer.shadowRadius = 10
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -34,8 +43,12 @@ class CanvasViewController: UIViewController {
             
             context.setBlendMode(.colorBurn)
             context.setLineCap(.round)
-            context.setLineWidth(4.8)
-            context.setStrokeColor(UIColor(red: 164.0/255.0, green: 176.0/255.0, blue: 245.0/255.0, alpha: 1.0).cgColor)
+            context.setLineWidth(14.8)
+            
+            let color = UIColor(red: 164.0/255.0, green: 176.0/255.0, blue: 245.0/255.0, alpha: 1.0).cgColor
+            context.setStrokeColor(color)
+            
+            context.setShadow(offset: CGSize.zero, blur: 10, color: color)
             context.strokePath()
             
             self.imgView.image = UIGraphicsGetImageFromCurrentImageContext()
